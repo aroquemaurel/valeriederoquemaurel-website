6615994b9a53d59296fdcad6d803b3c04b2b573c
Antoine de Roquemaurel <antoine.roquemaurel@gmail.com>
2017-11-06 00:34:21 +0100

    Initialize navigation model

--- navigation/models.py
+++ navigation/models.py
@@ -3,4 +3,9 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=256)
+    # TODO AR : parent, null ou une autre categorie
+    def __str__(self):
+        return self.title
