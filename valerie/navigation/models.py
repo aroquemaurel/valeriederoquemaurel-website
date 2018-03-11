1d3066f9a06f18bd228c2839aaaf1b1ef20b5ac1
Antoine de Roquemaurel <antoine.roquemaurel@gmail.com>
2018-03-11 19:06:54 +0100

    Ajout du type de catégorie.

--- valerie/navigation/models.py
+++ valerie/navigation/models.py
@@ -3,20 +3,30 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+class Type:
+    PHOTO = 1
+    CONTENT = 2
+
 
 class Category(models.Model):
     title = models.CharField(max_length=256)
     slug = models.SlugField(max_length=100)
     # Si aucun parent, catégorie, sinon sous-catégorie.
-    parent = models.ForeignKey('Category', null=True, related_name='parent_cat')
+    parent = models.ForeignKey('ParentCategory', null=True, related_name='parent_cat')
+    type = models.IntegerField(default=1)
+
+    def __str__(self):
+        return self.title
+
+
+class ParentCategory(Category):
     default_child = models.ForeignKey('Category', null=True, related_name='default_child_subcat')
-    home = models.BooleanField(default=False)
+    is_home = models.BooleanField(default=False)
 
     # Retourne les sous-catégories de la categorie courante
     def get_childs(self):
         return self.parent_cat.all()
 
 
-    def __str__(self):
-        return self.title
+
 
--- valerie/navigation/views.py
+++ valerie/navigation/views.py
@@ -5,28 +5,45 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-from valerie.navigation.models import Category
+from valerie.navigation.models import Category, Type, ParentCategory
+
 
 # Route
 def display_category(request, slug):
     slug_subcat = None
 
-    if slug is not None:
-        try:
-            cat = Category.objects.get(slug=slug)
+    try:
+        cat = ParentCategory.objects.get(slug=slug)
 
-            if cat.default_child is not None:
-                slug_subcat = cat.default_child.slug
-
-        except Category.DoesNotExist:
+        if cat.default_child is not None:
+            slug_subcat = cat.default_child.slug
+        else:
             slug_subcat = None
 
+    except ParentCategory.DoesNotExist:
+        slug_subcat = None
+
+    if slug_subcat is None and cat.get_childs().count() == 0:
+        return _display_category_without_childs(request, cat)
+    elif slug_subcat is None:
+        # TODO AR : On a des enfants, mais on connait pas la page par défaut
+        return None
+
     return display_subcategory(request, slug, slug_subcat)
+
+
+def _display_category_without_childs(request, cat):
+    # TODO AR : on devra afficher directement le contenu de la page.
+    return render(request, 'category_list.html', {'categories': ParentCategory.objects.all(),
+                                                  'current_cat': cat,
+                                                  'current_subcat': ''})
 
 
 def _display_category(request, cat):
     if cat is not None and cat.default_child is not None:
         subcat = cat.default_child
+    elif cat.get_childs().count() == 0:
+        return _display_category_without_childs(request, cat)
     else:
         subcat = None
 
@@ -34,14 +51,26 @@ def _display_category(request, cat):
 
 
 def _display_subcategory(request, cat, subcat):
-    return render(request, 'category_list.html', {'categories': Category.objects.filter(parent=None),
+    if subcat is None or cat is None:
+        # TODO AR : on balance une 404
+        pass
+
+    if subcat.type == Type.PHOTO:
+        # TODO AR : ?
+        pass
+    elif subcat.type == Type.CONTENT:
+        # TODO AR : ?
+        pass
+
+    return render(request, 'category_list.html', {'categories': ParentCategory.objects.all(),
                                                       'current_cat': cat,
                                                       'current_subcat': subcat})
+
 
 # Route
 def display_subcategory(request, slug_cat, slug_subcat):
     try:
-        cat = Category.objects.get(slug=slug_cat)
+        cat = ParentCategory.objects.get(slug=slug_cat)
     except Category.DoesNotExist:
         cat = None
 
@@ -56,9 +85,9 @@ def display_subcategory(request, slug_cat, slug_subcat):
 # Route
 def category_list(request):
     try:
-        slug_cat = Category.objects.get(home=True).slug
+        slug_cat = ParentCategory.objects.get(is_home=True).slug
     except Category.DoesNotExist:
-        slug_cat = ""
+        slug_cat = None
 
-    return display_category(request, None)
+    return display_category(request, slug_cat)
 
