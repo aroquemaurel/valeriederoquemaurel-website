2dd78fe6c09998b75de138a642261c8b9f909b17
Antoine de Roquemaurel <antoine.roquemaurel@gmail.com>
2018-03-25 12:40:57 +0200

    AJout de bootstrap et des statics

--- valerie/urls.py
+++ valerie/urls.py
@@ -14,10 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^', include('valerie.pages.urls')),
     url(r'^', include('valerie.users.urls')),
     url(r'^', include('valerie.navigation.urls')),
     url(r'^', include('valerie.photos_gallery.urls'))
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
