6fe8480504a1bf153e01c0c072022be2552ace72
Antoine de Roquemaurel <antoine.roquemaurel@gmail.com>
2018-09-02 22:55:31 +0200

    Models for events

--- valerie/events/models.py
+++ valerie/events/models.py
@@ -3,4 +3,11 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+
+class Event(models.Model):
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    location = models.TextField()
+    description = models.TextField()
+    
 # Create your models here.
