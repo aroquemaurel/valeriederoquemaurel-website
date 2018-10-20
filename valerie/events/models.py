df228bab192a296707062a3a98265adc8b814300
Antoine de Roquemaurel <antoine.roquemaurel@gmail.com>
2018-10-20 19:08:37 +0200

    Amélioration de l'affichage des dates, notamment des évenements

--- valerie/events/models.py
+++ valerie/events/models.py
@@ -1,14 +1,24 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from datetime import date
+
 from django.db import models
 
 
 class Event(models.Model):
-    start_date = models.DateTimeField()
-    end_date = models.DateTimeField()
+    start_date = models.DateField()
+    end_date = models.DateField()
     location = models.TextField()
     description = models.TextField()
+
+    @property
+    def is_ended(self):
+        return date.today() > self.end_date
+
+    @property
+    def is_now(self):
+        return self.start_date <= date.today() <= self.end_date
 
     def __str__(self):
         return 'from ' + self.start_date.strftime('%b %d %Y %I:%M%p')+\
