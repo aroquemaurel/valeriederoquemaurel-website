6009ea066be5e2e64795f27ea2af1a511495b78f
Antoine de Roquemaurel <aroquemaurel@roquemaurel.pro>
2019-12-25 19:35:35 +0100

    Update model of events #17

--- valerie/events/models.py
+++ valerie/events/models.py
@@ -4,9 +4,11 @@ from __future__ import unicode_literals
 from datetime import date
 
 from django.db import models
+from django.conf import settings
 
 
 class Event(models.Model):
+    title = models.CharField(max_length=256)
     start_date = models.DateField()
     end_date = models.DateField()
     location = models.TextField()
@@ -24,4 +26,14 @@ class Event(models.Model):
         return 'from ' + self.start_date.strftime('%b %d %Y %I:%M%p')+\
                ' to '+self.end_date.strftime('%b %d %Y %I:%M%p')+\
                ' at '+self.location
+
+    def get_images(self):
+        return self.article_image.all().order_by('position')
+
+
+class ImageEvent(models.Model):
+    img = models.ImageField(upload_to=settings.UPLOAD_RELATIVE_DIR + '/events')
+    position = models.PositiveIntegerField()
+    article = models.ForeignKey('events.Event', null=True, related_name='event_image',
+            on_delete=models.CASCADE)
 
