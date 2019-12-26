957c135d348c377de93250802cdd998c94fa260e
Antoine de Roquemaurel <aroquemaurel@roquemaurel.pro>
2019-12-27 00:08:56 +0100

    Add url in events template + refactoring with tags. #23

---
+++ valerie/templatetags/common_url.py
@@ -0,0 +1,33 @@
+from django import template
+from django.template.defaultfilters import stringfilter
+
+register = template.Library()
+
+
+@register.filter
+@stringfilter
+def youtube_embedded(text):
+    sp = text.split("youtube.com/watch?v=")
+    if len(sp) < 2:
+        return text
+
+    return '<iframe ' \
+           'width="560" ' \
+           'height="315" ' \
+           'src="https://www.youtube.com/embed/'+sp[1]+'" '\
+           'frameborder = "0" ' \
+           'allow = "autoplay; encrypted-media" ' \
+           'allowfullscreen></iframe>'
+
+
+@register.filter()
+@stringfilter
+def url_attachment(url):
+    return '<a href="'+url+'"><i class="glyphicon glyphicon-globe"></i> '+url+'</a>'
+
+
+@register.filter()
+@stringfilter
+def file_attachment(url, title):
+    return '<a href="/'+url+'"><i class="glyphicon glyphicon-file"></i> '+title+'</a>'
+
