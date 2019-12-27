a8e5b9417f5682c65144a65f22eec40a0dd49ffa
Antoine de Roquemaurel <aroquemaurel@roquemaurel.pro>
2019-12-27 19:16:41 +0100

    Refactoring images : create new model, data migrations

--- valerie/common/models.py
+++ valerie/common/models.py
@@ -1,3 +1,31 @@
-from django.db import models
+from abc import abstractmethod
 
-# Create your models here.
+from django.db import models
+from django.conf import settings
+
+
+class Attachment(models.Model):
+    @abstractmethod
+    def folder_name(self):
+        pass
+
+    def upload_path(self):
+        return settings.UPLOAD_RELATIVE_DIR + '/' + self.folder_name()
+
+    position = models.PositiveIntegerField()
+    title = models.CharField(max_length=256)
+
+
+class ImageAttachment(Attachment):
+    def folder_name(self):
+        pass
+
+    img = models.ImageField(upload_to=Attachment.upload_path)
+
+
+class DocumentAttachment(Attachment):
+    def folder_name(self):
+        pass
+
+    doc = models.FileField(upload_to=Attachment.upload_path)
+
