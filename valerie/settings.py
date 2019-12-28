b9972c506a862fe48cfeb21b2c74281b6bc973b7
Antoine de Roquemaurel <aroquemaurel@roquemaurel.pro>
2019-12-28 18:33:07 +0100

    Add Pillow in requirements and change language_code

--- valerie/settings.py
+++ valerie/settings.py
@@ -120,7 +120,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'fr-FR'
+LANGUAGE_CODE = 'fr'
 
 TIME_ZONE = 'UTC'
 
